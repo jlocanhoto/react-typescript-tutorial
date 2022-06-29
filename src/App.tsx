d8a6e8e571b75e17253d4bc7592797372de4e42e
João Canhoto <jlocanhoto@gmail.com>
2022-06-29 16:36:22 -0300

    Class 7 - Style Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
-import { Button } from './components/Button';
-import { Input } from './components/Input';
+import { Container } from './components/Container';
 
 import './App.css'
 
 function App() {
   return (
     <div className='App'>
-      <Button
-        handleClick={(event, id) => {
-          console.log('Button clicked', event, id);
-        }}
-      />
-      <Input value='' handleChange={(event) => { console.log(event); }} />
+      <Container styles={{ border: '1px solid black', padding: '1rem' }} />
     </div>
   )
 }
