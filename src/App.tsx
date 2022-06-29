a09e16e06f5b227d07759685ec82d4c7af545848
João Canhoto <jlocanhoto@gmail.com>
2022-06-29 16:23:49 -0300

    Class  6 - Event Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
-import { Greet } from './components/Greet';
-import { Heading } from './components/Heading';
-import { Oscar } from './components/Oscar';
-import { Status } from './components/Status';
+import { Button } from './components/Button';
+import { Input } from './components/Input';
 
 import './App.css'
 
 function App() {
   return (
     <div className='App'>
-      <Status status='error' />
-      <Heading>Placeholder text</Heading>
-      <Oscar>
-        <Heading>Oscar goes to Leonardo DiCaprio!</Heading>
-      </Oscar>
-      <Greet name='Canhoto' isLoggedIn={true} />
+      <Button
+        handleClick={(event, id) => {
+          console.log('Button clicked', event, id);
+        }}
+      />
+      <Input value='' handleChange={(event) => { console.log(event); }} />
     </div>
   )
 }
