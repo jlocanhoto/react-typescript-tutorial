ae7f01b54d0bdcae59bb80d3ee3dfb53f4ef8031
João Canhoto <jlocanhoto@gmail.com>
2022-06-28 22:22:38 -0300

    Class 4 - Basic Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,35 @@
 import { Greet } from './components/Greet';
 
 import './App.css'
+import { Person } from './components/Person';
+import { PersonList } from './components/PersonList';
 
 function App() {
+  const personName = {
+    first: 'Bruce',
+    last: 'Wayne'
+  }
+
+  const nameList = [
+    {
+      first: 'Bruce',
+      last: 'Wayne',
+    },
+    {
+      first: 'Clark',
+      last: 'Kent',
+    },
+    {
+      first: 'Princess',
+      last: 'Diana',
+    },
+  ]
+
   return (
     <div className="App">
-      <Greet name="Canhoto" />
+      <Greet name="Canhoto" messageCount={15} isLoggedIn={true} />
+      <Person name={personName} />
+      <PersonList names={nameList} />
     </div>
   )
 }
